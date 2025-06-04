96cf7eff4c133ffb0b08a3ef3925e75e278bb0db
Laboratorios <Laboratorios@R10>
2025-06-03 20:44:10 -0600

    Semana4

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.tienda.service;
+
+import com.tienda.domain.Categoria;
+import com.tienda.repository.CategoriaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CategoriaService {
+    //Anotacion para crear una unica instancia de respository y se crea automaticamente
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(){
+        var lista= categoriaRepository.findAll();
+        if(activo){
+            lista.removeIf(e-> !e.isActivo());
+        }
+        return lista;
+    }
+}
